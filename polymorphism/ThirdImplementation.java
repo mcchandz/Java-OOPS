13dcdeb084803638df3adbb2f9063149ec5a8de8
Chandana M C <135941935+mcchandz@users.noreply.github.com>
2024-08-02 23:22:09 +0530

    Add files via upload

---
+++ polymorphism/ThirdImplementation.java
@@ -0,0 +1,45 @@
+package polymorphism;
+
+public class ThirdImplementation {
+	Employee emp = new Devloper();
+	static void display(Employee emp) {
+		emp.work();
+	}
+	public static void main(String[] args) {
+		display(new Devloper());
+		display(new Tester());
+		
+		System.out.println("-----------------------------------------------------------------");
+		//specialization
+		Employee e1 = new Devloper();
+		e1.work();
+		Employee e2 = new Tester();
+		e2.work();
+		
+		System.out.println("-----------------------------------------------------------------");
+		//generalization
+		Employee emp;
+		emp = new Devloper();
+		emp.work();
+		emp = new Tester();
+		emp.work();
+	}
+
+}
+class Employee {
+	void work() {
+		System.out.println("working");
+	}
+}
+class Devloper extends Employee {
+	@Override
+	void work() {
+		System.out.println("developing app");
+	}
+}
+class Tester extends Employee {
+	@Override
+	void work() {
+		System.out.println("testing app");
+	}
+}
